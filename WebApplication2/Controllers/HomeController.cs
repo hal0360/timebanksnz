3d5fb0c2d7f6811ef64fa5972f0e5362a8a06cf6
hal0360 <hal0360@student.cpit.ac.nz>
2015-05-03 15:38:53 +1200

    Example timebank

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Timebanking()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Offers()
+        {       
+            return View();
+        }
+
+        public ActionResult Requests()
+        {         
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+    }
+}
